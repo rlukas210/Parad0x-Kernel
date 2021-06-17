f001d3082ff04f3c58b59dd799d4ab9a616acf14
rlukas210 <lucas.fran441@gmail.com>
2021-06-17 00:37:18 +0000

    kernel: hack to ignore magic mitsmatch [1/2]

--- kernel/module.c
+++ kernel/module.c
@@ -4261,6 +4261,9 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	if (err)
 		goto free_copy;
 
+	//FIXME
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
 	err = elf_header_check(info);
 	if (err)
 		goto free_copy;
